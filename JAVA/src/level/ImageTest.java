4f639f997c64c05c730a133767efd6ba2b6ff863
leesiyoung <ban00215@naver.com>
2015-10-31 19:14:13 +0900

    create gui & connect with PHP
    from lapto(SY_L) 15-10-31 19:15

---
+++ JAVA/src/level/ImageTest.java
@@ -0,0 +1,13 @@
+package level;
+import java.awt.*;
+import javax.swing.*;
+public class ImageTest extends JFrame{
+	ImageIcon explan;
+	JLabel back;
+
+		
+	public static void main(String args[]){
+		ImageTest I = new ImageTest();
+	}
+	
+}
